100c9ffb578d232453be89b67b7791703c51c1e0
jumpcutfindo <danielhoe97@gmail.com>
2025-03-21 13:01:14 +0800

    Reorganize input listener handling

--- src/main/java/com/jumpcutfindo/onmymark/client/InputListener.java
+++ src/main/java/com/jumpcutfindo/onmymark/client/InputListener.java
@@ -4,17 +4,13 @@ package com.jumpcutfindo.onmymark.client;
 import com.jumpcutfindo.onmymark.input.InputHandler;
 import com.jumpcutfindo.onmymark.input.OnPlayerMarkInputHandler;
 import com.jumpcutfindo.onmymark.network.ClientNetworkSender;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-@Environment(EnvType.CLIENT)
-public class InputListener implements ClientModInitializer {
+public class InputListener {
     private static final KeyBinding MARK_BINDING = KeyBindingHelper.registerKeyBinding(
             new KeyBinding(
                     "key.onmymark.mark",
@@ -33,21 +29,18 @@ public class InputListener implements ClientModInitializer {
                 )
     );
 
-    @Override
-    public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null) {
-                while (DEBUG_BINDING.wasPressed()) {
-                    ClientNetworkSender.createParty("New party name");
-                    ClientNetworkSender.leaveParty();
-                }
-
-                while (MARK_BINDING.wasPressed()) {
-                    // TODO: Add cooldown to marking
-                    InputHandler inputHandler = new OnPlayerMarkInputHandler();
-                    inputHandler.execute(client);
-                }
+    public void onInput(MinecraftClient client) {
+        if (client.player != null) {
+            while (DEBUG_BINDING.wasPressed()) {
+                ClientNetworkSender.createParty("New party name");
+                ClientNetworkSender.leaveParty();
             }
-        });
+
+            while (MARK_BINDING.wasPressed()) {
+                // TODO: Add cooldown to marking
+                InputHandler inputHandler = new OnPlayerMarkInputHandler();
+                inputHandler.execute(client);
+            }
+        }
     }
 }
--- src/main/java/com/jumpcutfindo/onmymark/client/OnMyMarkClientMod.java
+++ src/main/java/com/jumpcutfindo/onmymark/client/OnMyMarkClientMod.java
@@ -3,21 +3,27 @@ package com.jumpcutfindo.onmymark.client;
 import com.jumpcutfindo.onmymark.OnMyMarkMod;
 import com.jumpcutfindo.onmymark.graphics.OnMyMarkRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 
 public class OnMyMarkClientMod implements ClientModInitializer {
-    private ClientMarkerManager clientMarkerManager;
+    private InputListener inputListener;
     private OnMyMarkRenderer renderer;
+
+    private ClientMarkerManager clientMarkerManager;
 
     @Override
     public void onInitializeClient() {
-        this.clientMarkerManager = new ClientMarkerManager();
         this.renderer = new OnMyMarkRenderer(MinecraftClient.getInstance());
+        this.inputListener = new InputListener();
+
+        this.clientMarkerManager = new ClientMarkerManager();
 
         this.registerRenderer();
+        this.registerInputListener();
     }
 
     private void registerRenderer() {
@@ -30,5 +36,9 @@ public class OnMyMarkClientMod implements ClientModInitializer {
                         }
                 )
         );
+    }
+
+    private void registerInputListener() {
+        ClientTickEvents.END_CLIENT_TICK.register(inputListener::onInput);
     }
 }
