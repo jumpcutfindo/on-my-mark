7ed4a17b388a32cce4c6a7e4a0074702781c9e31
jumpcutfindo <danielhoe97@gmail.com>
2025-05-12 22:44:33 +0800

    Add predicate for color slider values

--- src/main/java/com/jumpcutfindo/onmymark/client/graphics/screen/party/MarkerColorWindow.java
+++ src/main/java/com/jumpcutfindo/onmymark/client/graphics/screen/party/MarkerColorWindow.java
@@ -9,10 +9,20 @@ import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MarkerColorWindow extends OnMyMarkWindow {
     public static final int WINDOW_WIDTH = 138, WINDOW_HEIGHT = 92;
     public static final int SLIDER_WIDTH = 128, SLIDER_HEIGHT = 18;
+
+    private static final Predicate<String> COLOR_VALUE_PREDICATE = (text) -> {
+        try {
+            int value = Integer.parseInt(text);
+            return value >= 0 && value <= 255;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    };
 
     private final ColorSlider redSlider, greenSlider, blueSlider;
     private final TextFieldWidget redField, greenField, blueField;
@@ -21,13 +31,16 @@ public class MarkerColorWindow extends OnMyMarkWindow {
         super(screen, Text.literal("Select a color"), WINDOW_WIDTH, WINDOW_HEIGHT);
 
         this.redSlider = new ColorSlider(SLIDER_WIDTH, SLIDER_HEIGHT, Text.literal("Red"), 0);
-        this.redField = new TextFieldWidget(screen.getTextRenderer(), 64, SLIDER_HEIGHT, Text.literal(Integer.toString(redSlider.getColorValue())));
+        this.redField = new TextFieldWidget(screen.getTextRenderer(), 36, SLIDER_HEIGHT, Text.literal(Integer.toString(redSlider.getColorValue())));
+        this.redField.setTextPredicate(COLOR_VALUE_PREDICATE);
 
         this.greenSlider = new ColorSlider(SLIDER_WIDTH, SLIDER_HEIGHT, Text.literal("Green"), 0);
-        this.greenField = new TextFieldWidget(screen.getTextRenderer(), 64, SLIDER_HEIGHT, Text.literal(Integer.toString(greenSlider.getColorValue())));
+        this.greenField = new TextFieldWidget(screen.getTextRenderer(), 36, SLIDER_HEIGHT, Text.literal(Integer.toString(greenSlider.getColorValue())));
+        this.redField.setTextPredicate(COLOR_VALUE_PREDICATE);
 
         this.blueSlider = new ColorSlider(SLIDER_WIDTH, SLIDER_HEIGHT, Text.literal("Blue"), 0);
-        this.blueField = new TextFieldWidget(screen.getTextRenderer(), 64, SLIDER_HEIGHT, Text.literal(Integer.toString(blueSlider.getColorValue())));
+        this.blueField = new TextFieldWidget(screen.getTextRenderer(), 36, SLIDER_HEIGHT, Text.literal(Integer.toString(blueSlider.getColorValue())));
+        this.blueField.setTextPredicate(COLOR_VALUE_PREDICATE);
     }
 
     @Override
@@ -47,7 +60,7 @@ public class MarkerColorWindow extends OnMyMarkWindow {
         blueSlider.setPosition(x, y + 48);
         blueSlider.render(context, mouseX, mouseY, 0);
         blueField.setPosition(x + SLIDER_WIDTH, y + 48);
-        redField.render(context, mouseX, mouseY, 0);
+        blueField.render(context, mouseX, mouseY, 0);
     }
 
     @Override
