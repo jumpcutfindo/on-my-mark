f2278320eeb7fb5f65609454f5e980c5d04fbb93
jumpcutfindo <danielhoe97@gmail.com>
2025-05-21 21:09:14 +0800

    Update mod version and configurations

--- src/main/java/com/jumpcutfindo/onmymark/OnMyMarkConfigModel.java
+++ src/main/java/com/jumpcutfindo/onmymark/OnMyMarkConfigModel.java
@@ -9,23 +9,29 @@ import io.wispforest.owo.config.annotation.*;
 public class OnMyMarkConfigModel {
 
     @SectionHeader("markerDrawingSection")
+    @Sync(Option.SyncMode.NONE)
     public ClampType markerClampType = ClampType.DEFAULT_CLAMP_TYPE;
 
     @RangeConstraint(min = 0, max = 100)
+    @Sync(Option.SyncMode.NONE)
     public int ovalClampPadding = ClampType.DEFAULT_OVAL_CLAMP_PADDING;
 
     @RangeConstraint(min = 256, max = 768)
+    @Sync(Option.SyncMode.NONE)
     public int circleClampDiameter = ClampType.DEFAULT_OVAL_CLAMP_PADDING;
 
     @SectionHeader("markerPointerSection")
 
     @RangeConstraint(min = 0.60F, max = 1.00F)
+    @Sync(Option.SyncMode.NONE)
     public float distanceLabelScale = MarkerRenderer.DEFAULT_LABEL_SCALE;
 
     @RangeConstraint(min = 4, max = 16)
+    @Sync(Option.SyncMode.NONE)
     public int markerPointerWidth = MarkerRenderer.DEFAULT_POINTER_WIDTH;
 
     @RangeConstraint(min = 4, max = 16)
+    @Sync(Option.SyncMode.NONE)
     public int markerPointerHeight = MarkerRenderer.DEFAULT_POINTER_HEIGHT;
 
     @RangeConstraint(min = 1, max = 86400)
